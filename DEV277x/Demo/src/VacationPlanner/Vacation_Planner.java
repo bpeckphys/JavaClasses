a5063a5dd82c2663aed921c19f3cde32d30f0232
Brenton Peck <34142746+bpeckphys@users.noreply.github.com>
2018-10-13 23:07:40 -0600

    Updated package structure

--- DEV277x/Demo/src/VacationPlanner/Vacation_Planner.java
+++ DEV277x/Demo/src/VacationPlanner/Vacation_Planner.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package demo.folder;
+package VacationPlanner;
 import java.util.*;
 
 /**
@@ -40,23 +40,27 @@ public class Vacation_Planner {
     public static void cost(){
         Scanner input = new Scanner(System.in);
         System.out.print("How many days are you going to spend travelling? ");
-        Integer durationDays = input.nextInt();
+        int durationDays = input.nextInt();
         
         System.out.print("How much money, in USD, are you planning to spend on your trip? ");
-        Integer spendingUSD = input.nextInt();
+        int spendingUSD = input.nextInt();
         
         System.out.print("What is the three letter currency symbol for your travel destination? ");
         String currencySymbol = input.next();
         
         System.out.print("How many " + currencySymbol + " are there in 1 USD? ");
-        Double currency = input.nextDouble();
+        double currency = input.nextDouble();
         
-        Integer durationHours = durationDays * 24;
-        Integer durationMinutes = durationHours * 60;
-        Double spendingDaily = spendingUSD * 1.0 / durationDays;
-        Double spendingCurrency = spendingUSD * currency;
-        Double spendingDailyCurrency = spendingCurrency / durationDays;
+        int durationHours = durationDays * 24;
+        int durationMinutes = durationHours * 60;
+        double spendingDaily = spendingUSD / durationDays;
+        double spendingCurrency = spendingUSD * currency;
+        double spendingDailyCurrency = spendingCurrency / durationDays;
         
+        // Round monetary values to 2 decimal places
+        spendingDaily = currencyRound(spendingDaily);
+        spendingCurrency = currencyRound(spendingCurrency);
+        spendingDailyCurrency = currencyRound(spendingDailyCurrency);
         
         System.out.println("If you are travelling for " + durationDays + " days that is the same as " + durationHours + " hours or " + durationMinutes + " minutes");
         System.out.println("If you are going to spend $" + spendingUSD + " that means per day you can spend up to $" + spendingDaily + " USD");
@@ -65,14 +69,21 @@ public class Vacation_Planner {
         System.out.println("");
     }
     
+    public static double currencyRound(double unrounded){
+        double rounded = unrounded * 100;
+        rounded = (int) rounded;
+        rounded = rounded / 100.0;
+        return rounded;
+    }
+    
     // This method takes in the time difference and displays the destination time
     public static void time(){
         Scanner input = new Scanner(System.in);
         System.out.print("What is the time difference, in hours, between your home and your destination? ");
-        Integer timeDif = input.nextInt();
+        int timeDif = input.nextInt();
         
-        Integer timeDestMidnight = timeDif % 24;
-        Integer timeDestNoon = (timeDif + 12) % 24;
+        int timeDestMidnight = timeDif % 24;
+        int timeDestNoon = (timeDif + 12) % 24;
         
         System.out.println("That means that when it is midnight at home it will be " + timeDestMidnight + ":00 in your travel destination");
         System.out.println("and when it is noon at home it will be " + timeDestNoon + ":00");
@@ -84,9 +95,10 @@ public class Vacation_Planner {
     public static void area(){
         Scanner input = new Scanner(System.in);
         System.out.print("What is the square area of your destination country in km2? ");
-        Integer areaKm = input.nextInt();
+        int areaKm = input.nextInt();
         
-        double areaMiles = areaKm * 0.621371 * 0.621371;
+        double areaMiles = areaKm * 0.62137 * 0.62137;
+        areaMiles = currencyRound(areaMiles);
         
         System.out.println("In miles2 that is " + areaMiles);
         System.out.println("***********");
