70ea4b1019f225ed777a2a7319a982b54cbc1105
bpeck <bpeck@squirrelshark.arbfund.com>
2018-10-25 08:42:32 -0600

    Handled multi-digit numbers and added annotation

--- DEV277x/ObjectOrientedIntro/src/FractionCalculator/Fraction.java
+++ DEV277x/ObjectOrientedIntro/src/FractionCalculator/Fraction.java
@@ -12,7 +12,11 @@ package FractionCalculator;
 public class Fraction {
     private int numerator;
     private int denominator;
+    public int myNum;
+    public int myDen;
+    public int otherNum;
     
+    // Returns a Fraction object
     public Fraction(int myNumerator, int myDenominator){
         if (myDenominator == 0){
             throw new IllegalArgumentException("Cannot have \"0\" as a denominator!");
@@ -25,73 +29,89 @@ public class Fraction {
         }
     }
     
+    // Overloaded Fraction object to handle whole numbers
     public Fraction(int myNumerator){
         this(myNumerator, 1);
     }
     
+    // Overloaded Fraction object to handle 0
     public Fraction(){
         this(0, 1);
     }
     
+    // Returns the numerator from the Fraction object
     public int getNumerator(){
         return this.numerator;
     }
     
+    // Returns the denominator from the Fraction object
     public int getDenominator(){
         return this.denominator;
     }
     
+    // Returns the Fraction object as a string
     public String toString(){
         return numerator + "/" + denominator;
     }
     
+    // Returns the Fraction object as a double value
     public double toDouble(){
         return ((double) numerator) / ((double) denominator);
     }
     
+    // Returns the sum of two Fraction objects
     public Fraction add(Fraction other){
         if(other.denominator == this.denominator){
-            numerator = this.numerator + other.numerator;
-            return new Fraction(numerator, denominator).toLowestTerms();
+            myNum = this.numerator + other.numerator;
+            return new Fraction(myNum, denominator).toLowestTerms();
         }else{
-            numerator = this.numerator * other.denominator;
-            other.numerator = other.numerator * this.denominator;
-            denominator = this.denominator * other.denominator;
-            numerator = this.numerator + other.numerator;
-            return new Fraction(numerator, denominator).toLowestTerms();
+            myNum = this.numerator * other.denominator;
+            otherNum = other.numerator * this.denominator;
+            myDen = this.denominator * other.denominator;
+            myNum = myNum + otherNum;
+            return new Fraction(myNum, myDen).toLowestTerms();
         }
     }
     
+    // Returns the difference of two Fraction objects
     public Fraction subtract(Fraction other){
         if(other.denominator == this.denominator){
-            numerator = this.numerator - other.numerator;
-            return new Fraction(numerator, denominator).toLowestTerms();
+            myNum = this.numerator - other.numerator;
+            return new Fraction(myNum, denominator).toLowestTerms();
         }else{
-            numerator = this.numerator * other.denominator;
-            other.numerator = other.numerator * this.denominator;
-            denominator = this.denominator * other.denominator;
-            numerator = this.numerator - other.numerator;
-            return new Fraction(numerator, denominator).toLowestTerms();
+            myNum = this.numerator * other.denominator;
+            otherNum = other.numerator * this.denominator;
+            myDen = this.denominator * other.denominator;
+            myNum = myNum - otherNum;
+            return new Fraction(myNum, myDen).toLowestTerms();
         }
     }
     
+    // Returns the product of two Fraction objects
     public Fraction multiply(Fraction other){
-        numerator = this.numerator * other.numerator;
-        denominator = this.denominator * other.denominator;
-        return new Fraction(numerator, denominator).toLowestTerms();
+        myNum = this.numerator * other.numerator;
+        myDen = this.denominator * other.denominator;
+        return new Fraction(myNum, myDen).toLowestTerms();
     }
     
+    // Returns the quotient of two Fraction objects
     public Fraction divide(Fraction other){
-        numerator = this.numerator * other.denominator;
-        denominator = this.denominator * other.numerator;
-        return new Fraction(numerator, denominator).toLowestTerms();
+        myNum = this.numerator * other.denominator;
+        myDen = this.denominator * other.numerator;
+        return new Fraction(myNum, myDen).toLowestTerms();
     }
     
+    // Returns a boolean of two Fraction objects are equal in reduced form
     public boolean equals(Object other){
         if (other instanceof Fraction){
+            System.out.println("This is an instance of Fraction");
             if(this.toLowestTerms() == ((Fraction) other).toLowestTerms()){
+                System.out.println("The reduced forms are equal"
+                        + "Reduced form of fraction 1 is: " + this.toLowestTerms() 
+                        + "Reduced form of fraction 2 is: " + ((Fraction) other).toLowestTerms());
                 return true;
             }else{
+                System.out.println("The reduced forms are NOT equal");
                 return false;
             }
         }else{
@@ -99,19 +119,23 @@ public class Fraction {
         }
     }
     
+    // Returns the reduced Fraction object
     public Fraction toLowestTerms(){
         int myGcd = gcd(numerator, denominator);
-        numerator = numerator / myGcd;
-        denominator = denominator / myGcd;
-        return new Fraction(numerator, denominator);
+        myNum = numerator / myGcd;
+        myDen = denominator / myGcd;
+        return new Fraction(myNum, myDen);
     }
     
+    // Returns the greatest common divisor of two integers
     public static int gcd(int numerator, int denominator){
-        while (numerator != 0 && denominator != 0){
-            int remainder = numerator % denominator;
-            numerator = denominator;
-            denominator = remainder;
+        int gcdNum = numerator;
+        int gcdDen = denominator;
+        while (gcdNum != 0 && gcdDen != 0){
+            int remainder = gcdNum % gcdDen;
+            gcdNum = gcdDen;
+            gcdDen = remainder;
         }
-        return numerator;
+        return gcdNum;
     }
 }
--- DEV277x/ObjectOrientedIntro/src/FractionCalculator/FractionCalculator.java
+++ DEV277x/ObjectOrientedIntro/src/FractionCalculator/FractionCalculator.java
@@ -11,44 +11,43 @@ import java.util.*;
  * @author bpeck
  */
 public class FractionCalculator {
+    static Boolean quit = false;
     
     public static void main(String[] args){
         intro();
         
         // Loop until user exits by pressing q
-        while (1 == 1){
+        while (!quit){
             String operation = getOperation();
             Fraction myFirstFraction = getFraction();
             Fraction mySecondFraction = getFraction();
             Fraction myResultFraction;
             Boolean myResult;
             
-            // Call relevant function according to operation input and output results
-            if (operation.matches("\\+")){
-                System.out.print(myFirstFraction + " + " + mySecondFraction + " = ");
+            // Call relevant Fraction function according to operation input and then output results
+            if (operation.toLowerCase().matches("q")){
+                quit = true;
+            }else if (operation.matches("\\+")){
                 myResultFraction = myFirstFraction.add(mySecondFraction);
-                System.out.println(myResultFraction);
+                System.out.println(myFirstFraction + " + " + mySecondFraction + " = " + myResultFraction);
             }else if (operation.matches("\\-")){
-                System.out.print(myFirstFraction + " - " + mySecondFraction + " = ");
                 myResultFraction = myFirstFraction.subtract(mySecondFraction);
-                System.out.println(myResultFraction);
+                System.out.println(myFirstFraction + " - " + mySecondFraction + " = " + myResultFraction);
             }else if (operation.matches("\\/")){
-                System.out.print(myFirstFraction + " / " + mySecondFraction + " = ");
                 myResultFraction = myFirstFraction.divide(mySecondFraction);
-                System.out.println(myResultFraction);
+                System.out.println(myFirstFraction + " / " + mySecondFraction + " = " + myResultFraction);
             }else if (operation.matches("\\*")){
-                System.out.print(myFirstFraction + " * " + mySecondFraction + " = ");
                 myResultFraction = myFirstFraction.multiply(mySecondFraction);
-                System.out.println(myResultFraction);
+                System.out.println(myFirstFraction + " * " + mySecondFraction + " = " + myResultFraction);
             }else if (operation.matches("\\=")){
-                System.out.print(myFirstFraction + " = " + mySecondFraction + " is ");
                 myResult = myFirstFraction.equals(mySecondFraction);
-                System.out.println(myResult);
+                System.out.println(myFirstFraction + " = " + mySecondFraction + " is " + myResult);
             }
             System.out.println("------------------------------------------------------------------------------");
         }
     }
     
+    // Welcome the user
     public static void intro(){
         System.out.println("This program is a fraction calculator/n "
                 + "It will add, subtract, multiply and divide fractions until you type Q to quit./n"
@@ -56,6 +55,8 @@ public class FractionCalculator {
                 + "------------------------------------------------------------------------------");
     }
     
+    
+    // Get the operation the user wishes to perform
     public static String getOperation(){
         System.out.println("Please enter an operation (+, -, /, *, = or Q to quit): ");
         Scanner input = new Scanner(System.in);
@@ -65,53 +66,65 @@ public class FractionCalculator {
             operation = input.next();
         }
         if (operation.toLowerCase().matches("q")){
-            System.exit(0);
+            quit = true;
         }
         return operation;
     }
     
+    // Check that the Fraction is a valid fraction
     public static boolean validFraction(String input){
+        // If input contains a "/" check that it doesn't start or end with it
         if (input.contains("/")){
             if (input.charAt(0) == '/' || input.endsWith("/")){
-                //System.out.println("Incorrect input, starts or ends with /");
+                System.out.println("Incorrect input, starts or ends with /");
                 return false;
             }
+            // Parse the fraction
             int fracLine = input.indexOf("/");
             String numerator = input.substring(0,fracLine);
             //System.out.println("The numerator is " + numerator);
             String denominator = input.substring(fracLine + 1, input.length());
             //System.out.println("The denominator is " + denominator);
+            
+            // Verify that the numerator and denominator are numbers
             if (!isNumber(numerator) || !isNumber(denominator)){
-                //System.out.println("Neither the numerator nor the denominator are numbers");
+                System.out.println("Either the numerator or the denominator are not a number");
                 return false;
             }else if (denominator.matches("0")){
-                //System.out.println("Cannot have a denominator of 0");
+                System.out.println("Cannot have a denominator of 0");
                 return false;
             }
             return true;
         }else{
+            // Check if the number is valid
             if (!isNumber(input)){
-                //System.out.println("String is not a number");
+                System.out.println("String is not a number");
                 return false;
             }
             return true;
         }
     }
     
+    // Get fraction input from user and gnerate Fraction from the parsed numerator and denominator
     public static Fraction getFraction(){
         int numerator;
         int denominator;
+        
+        // Get user input fraction
         System.out.println("Please enter a fraction (a/b) or integer (a): ");
         Scanner input = new Scanner(System.in);
         String myFraction = input.next();
+        
+        // Validate that user input is a valid fraction or a quit call
         while (!validFraction(myFraction)){
             System.out.println("Invalid fraction. Please enter (a/b) or (a), where a and b are integers and b is not zero: ");
             myFraction = input.next();
         }
         if (myFraction.toLowerCase().matches("q")){
-            System.exit(0);
+            quit = true;
         }
-        // Parse the numerator and denominator from the string
+        
+        // Parse the numerator and denominator from the string and return the Fraction
         if (!myFraction.contains("/")){
             numerator = Integer.parseInt(myFraction);
             return new Fraction(numerator);
@@ -127,6 +140,7 @@ public class FractionCalculator {
         }
     }
     
+    // Check if the input is a valid number
     public static boolean isNumber(String input){
         if (input.startsWith("-")){
             input = input.substring(1,input.length());
@@ -142,7 +156,7 @@ public class FractionCalculator {
         
         int i;
         for (i = 0; i < input.length(); i++){
-            if (!input.matches("[0123456789]")){
+            if (!input.substring(i, i + 1).matches("[0123456789]")){
                 return false;
             }
         }
